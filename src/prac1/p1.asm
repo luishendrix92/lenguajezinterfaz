7ceda77d8639d73ebc5ce091f1712bb751db9b53
luishendrix92 <luishendrix92@gmail.com>
2018-09-13 12:20:19 -0700

    p2 y p3 added

---
+++ src/prac1/p1.asm
@@ -0,0 +1,19 @@
+; 1 - Programa que muestra un caracter
+; López Garay Luis Felipe
+; 15211312
+; 12 de Septiembre del 2018
+
+.Model small
+
+.stack 64
+
+.Data
+
+.Code
+  mov dx,64
+  mov ah,02h
+  int 21h
+
+.Exit
+
+end
---
+++ src/prac2/p2.asm
@@ -0,0 +1,25 @@
+; 2 - Programa que imprime "Hola Mundo"
+; López Garay Luis Felipe
+; 15211312
+; 13 de Septiembre del 2018
+
+.Model small
+
+.stack 64
+
+.Data
+  msg db "Hola Mundo de LDI", 0Ah, 0Dh, "$"
+
+.Code
+  mov ax,seg msg ; Mover el mensaje al registro de datos
+  mov ds,ax
+  mov ah,09h ; Solicitar una impresión de cadena
+  lea dx,msg
+  int 21h    ; Mandar llamar a la interrupción IO
+
+  mov ah,4Ch ; Solicitar una terminación con return code
+  int 21h    ; Mandar a llamar a la interrupción IO
+
+.Exit
+
+end

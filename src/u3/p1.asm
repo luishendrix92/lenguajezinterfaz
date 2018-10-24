f28ac7b9d81d08e72703816f3fc277a71da1d7cd
Luis Felipe López G <luishendrix92@gmail.com>
2018-10-23 18:42:25 -0700

    Update p1.asm

--- src/u3/p1.asm
+++ src/u3/p1.asm
@@ -3,6 +3,48 @@
 ; 15211312
 ; 23 de Octubre del 2018
 
+INICIALIZAR MACRO
+  mov ax,@data
+  mov ds,ax
+ENDM
+
+IMPRIMIR_NOMBRE MACRO  
+  mov ah,09h
+  lea dx,nombre
+  int 21h
+ENDM
+
+IMPRIMIR_EDAD MACRO  
+  mov ah,09h
+  lea dx,edad
+  int 21h
+ENDM
+
+IMPRIMIR_NOCONTROL MACRO  
+  mov ah,09h
+  lea dx,nocontrol
+  int 21h
+ENDM
+
+IMPRIMIR_CARRERA MACRO  
+  mov ah,09h
+  lea dx,carrera
+  int 21h
+ENDM
+
+
+IMPRIMIR_HOBBY MACRO  
+  mov ah,09h
+  lea dx,hobby
+  int 21h
+ENDM
+
+IMPRIMIR_TRABAJO MACRO  
+  mov ah,09h
+  lea dx,trabajo
+  int 21h
+ENDM
+
 .model small
 
 .stack 64
@@ -16,34 +58,14 @@
   trabajo db "Soy desempleado :($"
 
 .code
-IMPRIMIR MACRO cadena
-  push ax
-  push dx
-  
-  mov ah,09h
-  lea dx,cadena
-  int 21h
-  
-  pop dx
-  pop ax
-ENDM
-
-INICIALIZAR MACRO
-  mov ax,@data
-  mov ds,ax
-ENDM
-
-MAIN PROC
   INICIALIZAR
   
-  IMPRIMIR nombre
-  IMPRIMIR edad
-  IMPRIMIR nocontrol
-  IMPRIMIR carrera
-  IMPRIMIR hobby
-  IMPRIMIR trabajo
+  IMPRIMIR_NOMBRE
+  IMPRIMIR_EDAD
+  IMPRIMIR_NOCONTROL
+  IMPRIMIR_CARRERA
+  IMPRIMIR_HOBBY
+  IMPRIMIR_TRABAJO
 
-  .exit
-ENDP
-
-end MAIN
+.exit
+end
